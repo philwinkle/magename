b6615fbe32f08be6a8993606de2398ba5164ecfe
Phillip Jackson <philwinkle@gmail.com>
2014-11-22 15:16:14 -0500

    Basic functionality fleshed out procedurally

--- public/index.php
+++ public/index.php
@@ -8,10 +8,19 @@ $twig = new Twig_Environment($loader);
 
 $app = new Silex\Application(); 
 
-$app['debug'] = true;
 
-$app->get('/', function() use ($app, $twig) {
-	return $twig->render('index.html');
+$file = ['../lib/dictionary/noun.txt','../lib/dictionary/verb.txt'][$termOrder = mt_rand(0,1)];
+
+$file = file_get_contents($file);
+$terms = explode("\n",$file);
+$term = ucfirst($terms[mt_rand(0,count($terms))]);
+
+$phrase = $termOrder ? 'Mage' . $term : $term . 'Gento';
+
+// $app['debug'] = true;
+
+$app->get('/', function() use ($app, $twig, $phrase) {
+	return $twig->render('index.html',array('phrase'=>$phrase));
 }); 
 
 $app->run();
