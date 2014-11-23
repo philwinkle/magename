afae51d3921944988ad7e31ce8c347607aaf9562
Phillip Jackson <philwinkle@gmail.com>
2014-11-22 19:59:26 -0500

    Reload terms via ajax

--- public/index.php
+++ public/index.php
@@ -1,5 +1,9 @@
 <?php
 
+//-------------------------------
+// BOOTSTRAP
+//-------------------------------
+
 require_once __DIR__.'/../vendor/autoload.php'; 
 
 $loader = new Twig_Loader_Filesystem(__DIR__.'/../templates');
@@ -7,7 +11,12 @@ $loader = new Twig_Loader_Filesystem(__DIR__.'/../templates');
 $twig = new Twig_Environment($loader);
 
 $app = new Silex\Application(); 
+//$app['debug'] = true;
 
+
+//-------------------------------
+// LOGIC
+//-------------------------------
 
 $file = ['../lib/dictionary/noun.txt','../lib/dictionary/verb.txt'][$termOrder = mt_rand(0,1)];
 
@@ -17,10 +26,22 @@ $term = ucfirst($terms[mt_rand(0,count($terms))]);
 
 $phrase = $termOrder ? 'Mage' . $term : $term . 'Gento';
 
-// $app['debug'] = true;
+
+
+//-------------------------------
+// ROUTES
+//-------------------------------
 
 $app->get('/', function() use ($app, $twig, $phrase) {
 	return $twig->render('index.html',array('phrase'=>$phrase));
 }); 
+
+$app->get('/get/phrase',function() use ($app, $twig, $phrase) {
+	return json_encode(array('phrase'=>$phrase));
+});
+
+//-------------------------------
+// UP UP AND AWAY
+//-------------------------------
 
 $app->run();
